9c9769270a2330c398d509ba7841768f7981bfd2
Thomas Garayua <tgarayua@gmail.com>
2020-09-16 13:28:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,4 +1,7 @@
 def find_element_index(array, value_to_find)
-  i = 0 
-  return value_to_find[i]
+  count = 0
+  while count < array.length do
+    puts array[count] == value_to_find
+    count += 1
+  end
 end
